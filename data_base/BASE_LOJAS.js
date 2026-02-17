d31893b2b75ccc42e4339bc5e24f7e14caca737c
Danilo Pires <danilopires189@gmail.com>
2026-02-17 08:07:03 -0300

    Automação: Atualização data_base - 17/02/2026  8:07:02,98

--- data_base/BASE_LOJAS.js
+++ data_base/BASE_LOJAS.js
@@ -1,5 +1,5 @@
 // BASE_LOJAS.js - Auto-convertido de BASE_LOJAS.xlsx
-// 16/02/2026, 20:05:17
+// 17/02/2026, 08:05:21
 
 window.DB_LOJAS = {
   BASE_LOJAS:   {
--- data_base/BASE_USUARIO.js
+++ data_base/BASE_USUARIO.js
@@ -1,5 +1,5 @@
 // BASE_USUARIO.js - Auto-convertido de BASE_USUARIO.xlsx
-// 16/02/2026, 20:05:18
+// 17/02/2026, 08:05:21
 
 window.DB_USUARIO = {"BASE_USUARIO":     [
         {
